3654b3d7022106e020384d65f0adbfe0a3318e0c
David Amedi <amukoahdavid@gmail.com>
2025-11-28 11:04:13 +0300

    create department model

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+#department - organizing courses and lectureres
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        return True
+    
+
+
+
+
+
+
+
+#course - linked to a department
+
+#Lecturer - associated to department
+
+#resourse - managing campus resources
+
+#Activities - student extra-cal activities
+
+#booking - resource reservation requests
